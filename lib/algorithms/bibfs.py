b3f185ac67b1cdb7143b35bee3dc493743624099
Segmentation Violator <segmentationviolator@proton.me>
2026-02-23 19:40:33 +0530

    fix(algorithms): improve bi-bfs by also stopping when either root-side or target-side stop adding cells to frontier

--- lib/algorithms/bibfs.py
+++ lib/algorithms/bibfs.py
@@ -17,14 +17,16 @@ class Turn(enum.IntEnum):
 
 
 class BiBFS(Algorithm):
-    __slots__: tuple[str, ...] = ("queue", "_visited")
+    __slots__: tuple[str, ...] = ("frontier_count", "queue", "_visited")
 
+    frontier_count: list[int]
     queue: collections.deque[tuple[int, Turn]]
     _visited: npt.NDArray[np.int8]
 
     def __init__(self, grid: Grid, root: int, target: int):
         super().__init__(grid, root, target)
 
+        self.frontier_count = [1, 1]
         self.queue = collections.deque(
             ((self.root, Turn.Root), (self.target, Turn.Target))
         )
@@ -42,10 +44,11 @@ class BiBFS(Algorithm):
 
     @typing.override
     def step(self) -> bool | None:
-        if len(self.queue) == 0:
+        if len(self.queue) == 0 or any(map(lambda count: count == 0, self.frontier_count)):
             return False
 
         cell, turn = self.queue.popleft()
+        self.frontier_count[turn] -= 1
 
         for neighbour in self.grid.neighbours(cell):
             visited = Turn(self._visited[neighbour])
@@ -76,6 +79,7 @@ class BiBFS(Algorithm):
                 return True
 
             self.queue.append((neighbour, turn))
+            self.frontier_count[turn] += 1
             self.parent[neighbour] = cell
             self._visited[neighbour] = turn
 
